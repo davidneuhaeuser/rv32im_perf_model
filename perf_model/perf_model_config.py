a872f06129e24d263b7c299ebf7fd4696438134f
DaNeu <daneu@pc.fritz.box>
2025-08-20 18:42:07 +0200

    improved config readability

--- perf_model/perf_model_config.py
+++ perf_model/perf_model_config.py
@@ -1,13 +1,6 @@
-################# DELAY CONFIG ##################
-# Select the same MEM_SIZE as used for
-# compilation (in linker file)
-MEM_SIZE = 4 * (2**20)  # default: 4*(2**20)
-
-# Select the size of the instruction memory used
-# for compilation (in linker file)
-DMEM_OFFSET = 0x100000  # default: 0x100000
-
-################# DELAY CONFIG ##################
+#################################################
+#                 DELAY CONFIG                  #
+#################################################
 # Select duration of memory read accesses
 READ_DELAY = 9  # default: 9
 
@@ -23,7 +16,10 @@ MULT_DELAY = 4  # default: 4
 # architecture
 JUMP_DELAY = 3
 
-################# CACHE CONFIG ##################
+
+#################################################
+#                 CACHE CONFIG                  #
+#################################################
 # Configuration of a set associative cache
 # using least recently used (CACHED = False
 # will ignore any effects of the cache)
@@ -38,3 +34,17 @@ BLOCK_SIZE = 2  # default: 2
 # Select an error correction factor for the
 # total number of clock cycles
 CACHE_ERROR_CORRECTION = 1.0  # default 1.0
+
+
+#################################################
+#     CHANGE VALUES BELOW ACCORDING TO          #
+#        `linker_rv32.ld` IF CHANGED            #
+#################################################
+
+# Select the same MEM_SIZE as used for
+# compilation (see: `linker_rv32.ld`)
+MEM_SIZE = 4 * (2**20)  # default: 4*(2**20)
+
+# Select the size of the instruction memory used
+# for compilation (see: `linker_rv32.ld`)
+DMEM_OFFSET = 0x100000  # default: 0x100000
